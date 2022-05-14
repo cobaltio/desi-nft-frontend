c2d0438bfa8f8987dff123fc111f85cf5cf1091e
Mayank Mamgain <mamgain.mayank01@gmail.com>
2022-05-14 23:05:19 +0530

    upload files

---
+++ static/dummyEvents.js
@@ -0,0 +1,26 @@
+export const dummyEvents = [
+  {
+    price: 0.1,
+    from: 'Braaga',
+    to: 'Mayank',
+    date: '9 days ago',
+  },
+  {
+    price: 0.098,
+    from: 'Huskbucket',
+    to: 'Braaga',
+    date: '7 days ago',
+  },
+  {
+    price: 0.103,
+    from: 'Atulya',
+    to: 'Huskbucket',
+    date: '4 days ago',
+  },
+  {
+    price: 0.15,
+    from: 'Mayank',
+    to: 'Atulya',
+    date: '1 days ago',
+  }
+]
